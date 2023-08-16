ad77d33a452422f5879074c3c911946d2d051fa4
uhi22 <uhi@hnng.de>
2023-08-16 12:42:07 +0200

    fixed nbrOfConversions

--- board_STM32F103/Core/Src/main.c
+++ board_STM32F103/Core/Src/main.c
@@ -210,7 +210,7 @@ static void MX_ADC1_Init(void)
   hadc1.Init.DiscontinuousConvMode = DISABLE;
   hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 5;
+  hadc1.Init.NbrOfConversion = 1;
   if (HAL_ADC_Init(&hadc1) != HAL_OK)
   {
     Error_Handler();

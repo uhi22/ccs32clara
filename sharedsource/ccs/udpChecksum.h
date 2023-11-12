1441c614c22f9ddebd058322eb18ea3b309a2bde
johannes <dev@johanneshuebner.com>
2023-11-12 18:39:16 +0100

    Removed old project

--- sharedsource/ccs/udpChecksum.h
+++
@@ -1,9 +0,0 @@
-/* Interface header for udpChecksum.c */
-
-/* Global Defines */
-
-/* Global Variables */
-
-/* Global Functions */
-
-extern uint16_t calculateUdpAndTcpChecksumForIPv6(uint8_t *UdpOrTcpframe, uint16_t UdpOrTcpframeLen, const uint8_t *ipv6source, const uint8_t *ipv6dest, uint8_t nxt);
